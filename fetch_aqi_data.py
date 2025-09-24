ccbdb347d17fb709db82b46574d0d3a495216f97
muhammadrafayasif <muhammadrafay3561@gmail.com>
2025-09-24 21:29:37 +0500

    Added HopsWorks integration

--- fetch_aqi_data.py
+++ fetch_aqi_data.py
@@ -1,21 +1,48 @@
 from playwright.sync_api import sync_playwright
+from datetime import datetime
+from dotenv import load_dotenv
 import pandas as pd
-import re, time
+import re, os, hopsworks
+
+# Load .env variables to communicate with HopsWorks
+load_dotenv()
+
+# Login to HopsWorks and get our feature store
+project = hopsworks.login(
+    api_key_value = os.getenv('HOPSWORKS_API_KEY')
+)
+fs = project.get_feature_store()
+
+# Get or create a feature group containing our features for AQI prediction
+fg = fs.get_or_create_feature_group(
+    name="aqi_feature_pipeline",
+    version=1,
+    primary_key=["timestamp"],
+    description="A feature pipeline for storing AQI, environmental pollutants and weather data to the feature store."
+)
 
 with sync_playwright() as p:
     browser = p.firefox.launch(headless=True)
-    page = browser.new_page()
+
+    # We set our locale to en-GB to prevent GitHub from using imperial units
+    context = browser.new_context(locale="en-GB")
+
+    page = context.new_page()
     page.goto(
         'https://www.accuweather.com/en/pk/karachi/261158/air-quality-index/261158',
     )
 
     # A template for a row in the dataset
     data = {
-        'hour': None,
+        'timestamp': None,
         'temp': None,
-        'wind_speed_km': None,
+        'wind_speed': None,
+        'wind_gusts': None,
         'humidity_percent': None,
-        'pressure_mb': None,
+        'dew_point': None,
+        'pressure': None,
+        'cloud_cover': None,
+        'visibility': None,
         'pm_10': None,
         'pm_2_5': None,
         'no_2': None,
@@ -27,11 +54,8 @@ with sync_playwright() as p:
 
     names = ['pm_10', 'pm_2_5', 'no_2', 'o_3', 'so_2', 'co']
 
-    # Get current hour in UTC
-    utc_hour = time.gmtime().tm_hour
-    # Convert to Pakistan Time (UTC+5)
-    pkt_hour = (utc_hour + 5) % 24
-    data['hour'] = pkt_hour
+    # Add current data and time to the new dataframe
+    data['timestamp'] = datetime.now().replace(minute=0, second=0, microsecond=0)
 
     # Add current AQI to the new dataframe
     AQI = page.locator('.aq-number').first
@@ -50,49 +74,28 @@ with sync_playwright() as p:
         'https://www.accuweather.com/en/pk/karachi/261158/current-weather/261158'
     )
 
-    cards = page.locator('.detail-item.spaced-content').all_inner_texts()
-    weather = ['wind_speed_km', 'humidity_percent', 'pressure_mb']
+    weather = ['wind_speed', 'wind_gusts', 'humidity_percent', 'dew_point', 'pressure', 'cloud_cover', 'visibility']
 
     # Add temperature data to the new dataframe
     TEMP = page.locator('.display-temp').first
     TEMP.wait_for(state='attached')
-    UNIT = TEMP.inner_text().split('°')[1]
     MAGNITUDE = float(TEMP.inner_text().split('°')[0])
-    if UNIT == 'F':
-        data['temp'] = round((MAGNITUDE-32) * 5/9 , 1)
-    else:
-        data['temp'] = round(MAGNITUDE, 1)
+    data['temp'] = round(MAGNITUDE, 1)
     
-    # Getting only the wind speed, humidity percentage and pressure (in mb)
-    cards = [ cards[n] for n, i in enumerate(cards) if 'Wind Gusts' in i or 'Indoor Humidity' in i or 'Pressure' in i ]
+    # Getting the temperature data required
+    cards = page.locator('.detail-item.spaced-content').all_inner_texts()
+    
+    # Only getting the required features we need, the following are not needed for our model
+    exclude = ["RealFeel", "Indoor Humidity", "Cloud Ceiling"]
+    cards = [ cards[n] for n, i in enumerate(cards) if not any(k in i for k in exclude)]
 
     for n, i in enumerate(cards):
-
-        # Convert from in to mb (international standard) for pressure and mph to km/h
-        # Converting imperial units to metric units
-        if weather[n] == 'pressure_mb':
-            MAGNITUDE = float(i.split()[2::][0])
-            UNIT = i.split()[2::][1]
-            if UNIT == 'in':
-                data[weather[n]] = round(MAGNITUDE * 33.8639, 1)
-            else:
-                data[weather[n]] = round(MAGNITUDE, 1)
-        elif weather[n] == 'wind_speed_km':
-            MAGNITUDE = float(i.split()[2])
-            UNIT = i.split()[3]
-            if UNIT == 'km/h':
-                data[weather[n]] = round(MAGNITUDE, 1)
-            else:
-                data[weather[n]] = round(MAGNITUDE * 1.609, 1)
-        else:
-            data[weather[n]] = round(float(re.findall(r'\d+', i)[0]), 1)
+        data[weather[n]] = round(float(re.findall(r'\d+', i)[0]), 1)
 
     # Convert python dictionary to Pandas DataFrame
     row = pd.DataFrame([data])
 
-    # Append data to CSV file
-    with open('aqi-data.csv', 'a') as f:
-        row = ','.join(str(value) for value in data.values())
-        f.write('\n'+row)
+    # Append current data to our feature store
+    fg.insert(row, wait=True)
 
     browser.close()
